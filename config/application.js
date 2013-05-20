c08a5f28aa4a029def0b9fcbaeaa3a5916f8558c
usp <ususso@gmail.com>
2013-05-20 16:30:52 +0900

    initial config

--- config/application.js
+++ config/application.js
@@ -1,10 +1,10 @@
 module.exports = {
 	
 	// Name of the application (used as default <title>)
-	appName: "Sails Application",
+	appName: "checkout",
 
 	// Port this Sails application will live on
-	port: 1337,
+	port: 1339,
 
 	// The environment the app is deployed in 
 	// (`development` or `production`)
